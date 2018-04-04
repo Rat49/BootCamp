71b7f5b3cc6f479ff61f7082d90ad5a3088d50c6
yuyoyuppe <a.yuyoyuppe@gmail.com>
2018-04-05 01:57:02 +0300

    Statically link SFML + composite lib

--- Source/Asteroids/main.cpp
+++ Source/Asteroids/main.cpp
@@ -2,27 +2,27 @@
 
 int main()
 {
-	sf::RenderWindow rw(sf::VideoMode::getDesktopMode(), "Asteroids");
-	sf::Event sysEvent;
-	while (rw.isOpen())
-	{
-		if (rw.pollEvent(sysEvent))
-		{
-			if (sysEvent.type == sysEvent.KeyPressed && sysEvent.key.code == sf::Keyboard::Escape)
-			{
-				rw.close();
-			}
-		}
+  sf::RenderWindow rw(sf::VideoMode::getDesktopMode(), "Asteroids");
+  sf::Event        sysEvent;
+  while(rw.isOpen())
+  {
+    if(rw.pollEvent(sysEvent))
+    {
+      if(sysEvent.type == sysEvent.KeyPressed && sysEvent.key.code == sf::Keyboard::Escape || sf::Event::Closed)
+      {
+        rw.close();
+      }
+    }
 
-		//Input update
-		//Audio update
-		//Logic update
-		//...
+    //Input update
+    //Audio update
+    //Logic update
+    //...
 
-		rw.clear();
-		//Rendering update
-		rw.display();
-	}
+    rw.clear();
+    //Rendering update
+    rw.display();
+  }
 
-	return 0;
+  return 0;
 }
