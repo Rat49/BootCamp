13e7beb66aaedbbe48b7a720374ef5c6cd70542e
Solnec <ks_9580@mail.ru>
2018-04-04 18:09:00 +0300

    create class ResourseManager

---
+++ project/ResourseManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ResourseManager.h"
+
+
+ResourseManager::ResourseManager()
+{
+}
+
+
+ResourseManager::~ResourseManager()
+{
+}
---
+++ project/ResourseManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class ResourseManager
+{
+public:
+	ResourseManager();
+	~ResourseManager();
+};
+
