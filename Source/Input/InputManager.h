5f02c5e1bca5375faa0cd86b7f71d3420a6c729a
Olga Gasanova <olga.gasanova@sperasoft.com>
2018-04-06 14:51:59 +0300

    New project structure

---
+++ Source/Input/InputManager.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "SFML\Window.hpp"
+#include <map>
+
+enum class GameActions {
+	Up,
+	Down,
+	Left,
+	Right,
+	Escape,
+	Enter,
+	Space
+};
+
+enum class ButtonsState {
+	JustPressed,
+	Pressed,
+	JustReleased,
+	Released
+};
+
+class InputManager
+{
+private:
+	std::map<int, int> buttonsState;
+public:
+	std::map<int, int> GetState() const;
+	std::map<int,int> buttonsKey;
+	int ChangeStateWhenRelease(int);
+	int ChangeStateWhenPressed(int);
+	void Update();
+	InputManager(const std::map<int, int>& arr);
+	~InputManager();
+};
+
