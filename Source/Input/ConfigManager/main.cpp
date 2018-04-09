7d7fb8c310a83ec0ecd3821552443c242a7dd411
Klaudia Rozycka <klaudia.angelina.rozycka@sperasoft.com>
2018-04-09 15:08:14 +0200

    Fixed ConfigManager::createCategories

--- Source/Input/ConfigManager/main.cpp
+++ Source/Input/ConfigManager/main.cpp
@@ -2,14 +2,9 @@
 
 
 int main() {
-	/*
-	std::string line = "ffgaa [dhswrra]";
-	std::string value;
-	std::regex pattern("\[[A-Za-z]*\]");
+	
 
-	if (std::regex_search(line, value, pattern)) {
-		std::string categoryName = value.substr(1, value.size() - 2);
+	//ConfigManager cm;
 
-		std::cout << categoryName << std::endl;
-	}*/
+	//cm.createCategories();
 }
