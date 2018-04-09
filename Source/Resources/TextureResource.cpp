cf5ffedd0ad7957d673e9ac0f227709c30716f12
ludmila.avdushkina <lyudmila.avd@gmail.com>
2018-04-09 17:58:02 +0300

    Add class TextureResource

--- Source/Resources/TextureResource.cpp
+++ Source/Resources/TextureResource.cpp
@@ -1,37 +1,38 @@
-#include "PictureResource.h"
-#include <cassert>
+#include "TextureResource.h"
 
-PictureResource::PictureResource(const std::string& id, const std::string& name):
-	Resource(id, name),
+TextureResource::TextureResource(const std::string& id, const std::string& name) :
+	Resource(id, name), 
 	_resource(nullptr)
 {
+
 }
 
-void PictureResource::Load() 
+void TextureResource::Load()
 {
-	_resource = new sf::Image();
+	_resource = new sf::Texture();
 	bool isLoadSuccess = _resource->loadFromFile(_fullName);
+
 	if (!isLoadSuccess)
 	{
 		_resource = nullptr;
 	}
+
 	assert(isLoadSuccess && "Resource can not be loaded");
 }
 
-void PictureResource::Unload()
+void TextureResource::Unload()
 {
 	delete(_resource);
 	_resource = nullptr;
 }
 
-sf::Image& PictureResource::Get()
+sf::Texture& TextureResource::Get()
 {
 	assert(GetRefCounter() != 0 && "Resource references not found");
 	return *_resource;
 }
 
-
-PictureResource::~PictureResource()
+TextureResource::~TextureResource()
 {
-	
+
 }
