61b87572a36094953f6c4b8521aad6a3c0a6474a
ludmila.avdushkina <lyudmila.avd@gmail.com>
2018-04-10 18:00:14 +0300

    AnimationPlayer realization (version for test)

--- Source/Animation/AnimationPlayer.cpp
+++ Source/Animation/AnimationPlayer.cpp
@@ -1,58 +1,70 @@
 #include "AnimationPlayer.h"
 #include "ResourceManager.h"
 
-AnimationPlayer::AnimationPlayer(AnimationResource& animation, bool isLooped) :
-	_animation(animation),
+AnimationPlayer::AnimationPlayer(sf::Sprite* animatedSprite, const std::vector<sf::Texture>& spriteSheet, bool isLooped) :
+	_animatedSprite(animatedSprite),
+	_spriteSheet(spriteSheet),
 	_isLooped(isLooped),
-	_animationTime(animation.getAnimationTime()),
-	_activeFrameId(0)
+	//_animationTime(animation.getAnimationTime()),
+	_currentFrame(0),
+	_isStoped(true),
+	_framesNumber(std::abs(std::distance(_spriteSheet.end(), _spriteSheet.begin()))),
+	_animationTime(sf::seconds(0.2)),
+	_frameTime(sf::seconds(_animationTime.asSeconds / static_cast<float>(_framesNumber)))
 {
-
+	_clock.getElapsedTime();
 }
+
 
 void AnimationPlayer::Start()
 {
-	_animationTime = _animation.getAnimationTime();
+	//_animationTime = _animation.getAnimationTime();
 	_isStoped = false;
+	_clock.restart();
 }
 
 void AnimationPlayer::Start(sf::Time animationTime)
 {
 	_animationTime = animationTime;
 	_isStoped = false;
+	_clock.restart();
 }
 
 void AnimationPlayer::Pause()
 {
 	_isStoped = true;
+	_clock.restart();
 }
 
 void AnimationPlayer::Reset()
 {
-	_activeFrameId = 0;
+	_currentFrame = 0;
 	_isStoped = true;
 }
 
 void AnimationPlayer::Update()
 {
-
-}
-
-void AnimationPlayer::draw(sf::RenderTarget & target, sf::RenderStates states)
-{
-	//target.draw();
-}
-
-AnimationResource& AnimationPlayer::GetAnimation() const
-{
-	return _animation;
-}
-
-void AnimationPlayer::SetAnimation(AnimationResource animation)
-{
-	_animation = animation;
-	_animationTime = animation.getAnimationTime();
-	_frameTime = _animationTime.asSe
+	if (!_isStoped)
+	{
+		unsigned int frameNumber = _currentFrame + std::ceil(_clock.getElapsedTime().asSeconds / _frameTime.asSeconds());
+		if (_isLooped)
+		{
+			_currentFrame = frameNumber % _framesNumber;
+			_animatedSprite->setTexture(_spriteSheet[_currentFrame]);
+		}
+		else
+		{
+			if (frameNumber < _framesNumber)
+			{
+				_currentFrame = frameNumber;
+				_animatedSprite->setTexture(_spriteSheet[_currentFrame]);
+			}
+			else
+			{
+				Reset();
+			}
+		}
+	}
 }
 
 void AnimationPlayer::SetLooped(bool isLooped)
--- Source/Animation/AnimationPlayer.h
+++ Source/Animation/AnimationPlayer.h
@@ -5,26 +5,30 @@
 class AnimationPlayer : public sf::Drawable
 {
 public:
-	AnimationPlayer(AnimationResource& animation, bool isLooped);
+	AnimationPlayer(sf::Sprite* animatedSprite, const std::vector<sf::Texture>& spriteSheet, bool isLooped);
 
-	AnimationResource& GetAnimation() const;
-	void SetAnimation(AnimationResource animation);
+	//AnimationResource& GetAnimation() const;
+	//void SetAnimation(AnimationResource animation);
+	//void SetSpriteSheet();
 	void Start();
 	void Start(sf::Time AnimationTime);
 	void Pause();
 	void Reset();
-	void update();
-	void draw(sf::RenderTarget& target, sf::RenderStates states);
+	void Update();
 	void SetLooped(bool isLooped);
 
 	~AnimationPlayer();
 
 private:
-	AnimationResource& _animation;
-	sf::Sprite _unimationSprite;
-	unsigned int _activeFrameId;
+	//AnimationResource& _animation;
+	const std::vector<sf::Texture>& _spriteSheet;
+	sf::Sprite* _animatedSprite;
+	unsigned int _currentFrame;
 	bool _isLooped;
 	bool _isStoped;
+	unsigned int _framesNumber;
 	sf::Time _animationTime;
 	sf::Time _frameTime;
+	sf::Time _currentTime;
+	sf::Clock _clock;
 };
