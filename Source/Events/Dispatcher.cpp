72198dca06f992c087501c9f5aa72a3119782f5c
Ольга <ek.stepano@yandex.ru>
2018-04-10 14:51:13 +0300

    changes were made according last comments

--- Source/Events/Dispatcher.cpp
+++ Source/Events/Dispatcher.cpp
@@ -14,7 +14,6 @@ Dispatcher::~Dispatcher()
 Dispatcher& Dispatcher::getInstance()
 {
 	static Dispatcher instance;
-
 	return instance;
 }
 
@@ -22,15 +21,16 @@ int Dispatcher::Connect(const EventID_t eventID, const EventHandler_t& handler)
 {
 	std::map<Token_t, EventHandler_t>& handlersForCurrentEvent = _listeners[eventID];
 	Token_t tokenForCurrentListener = 0;
+
 	if (handlersForCurrentEvent.empty())
 	{
-		handlersForCurrentEvent.insert(std::pair<Token_t, EventHandler_t> (tokenForCurrentListener, handler));
+		handlersForCurrentEvent[tokenForCurrentListener] = handler;
 		return tokenForCurrentListener;
 	}
+
 	Token_t lastValidTokenForCurrentEvent = handlersForCurrentEvent.rbegin()->first;
 	tokenForCurrentListener = lastValidTokenForCurrentEvent + 1;
-	handlersForCurrentEvent.insert(std::pair<Token_t, EventHandler_t>(tokenForCurrentListener, handler));
-
+	handlersForCurrentEvent[tokenForCurrentListener] = handler;
 	return tokenForCurrentListener;
 }
 
@@ -53,25 +53,24 @@ void Dispatcher::Send(const Event& event, EventID_t eventID)
 
 void Dispatcher::Disconnect(const EventID_t eventID, const Token_t token)
 {
-	if (_listeners.empty())
+	std::map<EventID_t, std::map <Token_t, EventHandler_t>>::iterator listenersForCurrentEvent = _listeners.find(eventID);
+
+	if (listenersForCurrentEvent == _listeners.end())
 	{
-		std::cout << DispatcherErrorMessagesStore::disconectFailedNoListenersToDisconect << std::endl;
+		std::cout << DispatcherErrorMessagesStore::disconectFailedNoListenersForThisEvent << std::endl;
 		return;
 	}
+	
+	auto positionOfRequiredListener = listenersForCurrentEvent->second.find(token);
 
-	std::map <Token_t, EventHandler_t>& listenersForCurrentEvent = _listeners.find(eventID)->second;
-	auto positionOfRequiredListener = listenersForCurrentEvent.find(token);
-
-	if (positionOfRequiredListener == listenersForCurrentEvent.cend())
+	if (positionOfRequiredListener == listenersForCurrentEvent->second.end())
 	{
 		std::cout << DispatcherErrorMessagesStore::disconectFailedWrongToken << std::endl;
 		return;
 	}
 	else
 	{
-		listenersForCurrentEvent.erase(positionOfRequiredListener);
-		return;
+		listenersForCurrentEvent->second.erase(positionOfRequiredListener);
 	}
-
 }
 
--- Source/Events/DispatcherErrorMessagesStore.h
+++ Source/Events/DispatcherErrorMessagesStore.h
@@ -2,7 +2,7 @@
 
 struct DispatcherErrorMessagesStore
 {
-	static constexpr const char* disconectFailedNoListenersToDisconect = "Failed to disconnect. The dispatcher has no listeners.";
+	static constexpr const char* disconectFailedNoListenersForThisEvent = "Failed to disconnect. The dispatcher has no listeners for this event.";
 
 	static constexpr const char* disconectFailedWrongToken = "Failed to disconnect. Wrong token. The dispatcher has no listeners with this token.";
 };
