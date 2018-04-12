f9205dcae6228d0b7beb14debe8fbf091d1c8afc
yuyoyuppe <a.yuyoyuppe@gmail.com>
2018-04-13 01:05:45 +0300

    AnimationManager + new Resource type!

---
+++ Source/AnimationTest/AnimationTest.cpp
@@ -0,0 +1,48 @@
+#include "AnimationPlayer.h"
+#include "ResourceManager.h"
+
+int main()
+{
+	ResourceManager *rm = new ResourceManager();
+	int counterPicture = 0;
+	int counterImageSequence = 0;
+
+	ImageSequenceResource* girl = rm->GetResource<ImageSequenceResource>("girl");
+	counterImageSequence = girl->GetRefCounter();
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	window.clear();
+	sf::Sprite* sprite = new sf::Sprite();
+
+	AnimationPlayer* animationPlayer = new AnimationPlayer(sprite, girl, true);
+
+	animationPlayer->Start(sf::milliseconds(10.0f));
+	sf::Clock clock;
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+			{
+				animationPlayer->Reset();
+			}
+
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::BackSpace)
+			{
+				animationPlayer->Reset();
+				animationPlayer->SetLooped(false);
+				animationPlayer->Start();
+			}
+		}
+
+		animationPlayer->Update(clock.getElapsedTime());
+	
+		window.clear();
+		window.draw(*sprite);
+		window.display();
+		clock.restart();
+	}
+
+	return 0;
+}
