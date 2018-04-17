01e9e7676f399a1425a9d29faac3c018e5ed903b
Klaudia Rozycka <klaudia.angelina.rozycka@sperasoft.com>
2018-04-17 16:16:51 +0200

    Added Login methods and GetLeaderboard method

--- Source/LeaderBoardTest/LeaderBoard.cpp
+++ Source/LeaderBoardTest/LeaderBoard.cpp
@@ -1,15 +1,60 @@
 #include"LeaderBoard.h"
 
+using namespace PlayFab;
+using namespace ClientModels;
+//using namespace AdminModels;
 
 
-
-LeaderBoard* LeaderBoard::Create() {
+Leaderboard* Leaderboard::Create() {
 	
-	return new LeaderBoard();
+	return new Leaderboard();
 
 }
-
+/*
 void LeaderBoard::CreatePlayerStatisticDefinition() {
+	/*
+	CreatePlayerStatisticDefinitionRequest request;
+	request.StatisticName = "Top Score";
+	request.VersionChangeInterval = StatisticResetIntervalOption::StatisticResetIntervalOptionWeek;
+
+	ProcessApiCallback<CreatePlayerStatisticDefinitionResult> callback;
+
+	PlayFabAdminAPI::CreatePlayerStatisticDefinition(request, callback);
+}
+
+void LeaderBoard::UpdatePlayerStatisticDefinition() {
+
+}
+*/
+
+void Leaderboard::LoginWithCustomID() {
+
+	LoginWithCustomIDRequest request;
+
+	//PlayFabClientAPI::LoginWithCustomID(request, callback, errorCallback);
+}
+
+void Leaderboard::LoginWithEmailAddress(std::string emailAddress) {
+
+	LoginWithEmailAddressRequest request;
+	request.Email = emailAddress;
+	ProcessApiCallback<LoginResult> callback;
+	ErrorCallback errorCallback;
+
+	PlayFabClientAPI::LoginWithEmailAddress(request, callback, errorCallback);
+}
+
+void Leaderboard::Register() {
 
 }
 
+void Leaderboard::GetLeaderboard() {
+
+	GetLeaderboardRequest request;
+	request.StatisticName = "Top Score";
+
+	ProcessApiCallback<GetLeaderboardResult> callback;
+	ErrorCallback errorCallback;
+
+	PlayFabClientAPI::GetLeaderboard(request, callback, errorCallback);
+}
