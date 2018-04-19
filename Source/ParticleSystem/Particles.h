e38c184e4ae40cdb380002d38a22c70c6ec28dba
Olga Gasanova <olga.gasanova@sperasoft.com>
2018-04-19 19:55:32 +0300

    Direction of particles depends on emitter velocity

--- Source/ParticleSystem/Particles.h
+++ Source/ParticleSystem/Particles.h
@@ -5,13 +5,18 @@
 #include <cassert>
 #include <SFML/Graphics.hpp>
 
-class Particle
+struct Particle
 {
-public:
 	sf::Vector2f velocity;
 	sf::Time AllLifetime;
 	sf::Time lifetime;
 };
+
+enum Distribution {
+	Normal,
+	Uniform
+};
+
 template<class T, class Compare>
 constexpr const T& clamp(const T& v, const T& lo, const T& hi, Compare comp)
 {
@@ -19,17 +24,11 @@ constexpr const T& clamp(const T& v, const T& lo, const T& hi, Compare comp)
 		comp(v, lo) ? lo : comp(hi, v) ? hi : v;
 }
 
-
-	template<class T>
-	constexpr const T& clamp(const T& v, const T& lo, const T& hi)
-	{
-		return clamp(v, lo, hi, std::less<>());
-	}
-
-
-
-typedef std::uniform_real_distribution<> UniRealDist;
-typedef std::uniform_int_distribution<> UniIntDist;
+template<class T>
+constexpr const T& clamp(const T& v, const T& lo, const T& hi)
+{
+	return clamp(v, lo, hi, std::less<>());
+}
 
 class ParticleSystem : public sf::Drawable, public sf::Transformable
 {
@@ -40,7 +39,8 @@ public:
 		m_vertices(sf::Points, count),
 		m_lifetime(sf::seconds(3)),
 		m_emitter(0, 0), 
-		m_canvasSize(canvasSize)
+		m_canvasSize(canvasSize),
+		emitterVelocity(0,0)
 	{
 		fuel();
 	}
@@ -54,7 +54,7 @@ public:
 			float speed = 0;
 			p.velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
 			m_vertices[i].position = m_emitter;
-			m_particles[i].lifetime = sf::milliseconds((std::rand() % 4000));
+			m_particles[i].lifetime = sf::milliseconds((std::rand() % _lifetime));
 		}
 	}
 	void setEmitter(sf::Vector2f position)
@@ -64,6 +64,12 @@ public:
 
 	void AddCircleForce(sf::Vector2f centre, float radius, float strenght) {
 		_forces.insert(_forces.begin(), circleForce{ strenght, centre, radius });
+	}
+
+	void AddCircleForceForRocket() {
+		AddCircleForce(m_emitter - sf::Vector2f(2 * emitterVelocity.x - emitterVelocity.y + 15, 2 * emitterVelocity.y + emitterVelocity.x + 30), 20, -0.5);
+		AddCircleForce(m_emitter - sf::Vector2f(4 * emitterVelocity.x + emitterVelocity.y + 10, 4 * emitterVelocity.y - emitterVelocity.x + 10), 20, -0.5);
+		AddCircleForce(m_emitter - sf::Vector2f(6 * emitterVelocity.x - emitterVelocity.y + 15, 6 * emitterVelocity.y + emitterVelocity.x +25), 20, -0.5);
 	}
 	void ChangeColor(const float lifeTime, sf::Vertex& vertex) {
 		std::pair<float, sf::Color> tempColor (1.f, sf::Color::Black);
@@ -83,8 +89,8 @@ public:
 
 	void update(sf::Time elapsed)
 	{
-		//std::cout << "update" << std::endl;
 		int count = 0;
+		
 		for (std::size_t i = 0; i < m_particles.size(); ++i)
 		{
 			Particle& p = m_particles[i];
@@ -95,7 +101,7 @@ public:
 
 			float ratio = clamp(p.lifetime.asSeconds() / m_lifetime.asSeconds(), 0.f, 1.f);
 			m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
-			if (p.lifetime <= sf::Time::Zero && count <= _rate)
+			if (p.lifetime <= sf::Time::Zero && count < _rate)
 			{
 				resetParticle(i);
 				count++;
@@ -119,28 +125,39 @@ public:
 				|| m_vertices[i].position.x < 0
 				|| m_vertices[i].position.y > m_canvasSize.y
 				|| m_vertices[i].position.y < 0
-				|| m_vertices[i].color.a < 10) && count <= _rate)
+				|| m_vertices[i].color.a < 10) && count < _rate)
 			{
 				resetParticle(i);
 				count++;
 			}
 		}
+
 		for (auto& force : _forces) {
 			
 			force.centre += emitterVelocity * elapsed.asSeconds();
 		}
 		m_emitter+= emitterVelocity * elapsed.asSeconds();
-		//_emitterAngle; 
 	}
+
+
 	void setEmitterPosition(sf::Vector2f position) { m_emitter = position; }
 	void setSimpleForce(sf::Vector2f force) { _simpleForce = force; }
-	void setEmitterVelocity(sf::Vector2f velocity) { 
-		emitterVelocity = velocity; 
-		/*AddCircleForce(m_emitter-sf::Vector2f(2* velocity.x+3, 2 * velocity.y), 20, -3);
-		AddCircleForce(m_emitter - sf::Vector2f(3 * velocity.x - 3, 3 * velocity.y), 20, -3);
-		AddCircleForce(m_emitter - sf::Vector2f(4 * velocity.x + 3, 4 * velocity.y), 20, -3);*/
-	}
+	void setEmitterVelocity(sf::Vector2f velocity) { emitterVelocity = velocity; }
 	void setRate(int rate) { _rate = rate; }
+
+	void setNormalDistrParams(float mean, float sigma){
+		_mean = mean;
+		_sigma = sigma;
+	}
+
+	void setUniformDistrRange(int range){
+		_range = range;
+	}
+
+	sf::Vector2f m_emitter;
+	sf::Vector2f emitterVelocity;
+	Distribution distribution = Distribution::Normal;
+
 private:
 
 
@@ -155,13 +172,13 @@ private:
 			sf::CircleShape shape(force.radius);
 			shape.setFillColor(sf::Color(150, 50, 250));
 			shape.setPosition(force.centre);
-			shape.setFillColor(sf::Color(static_cast<sf::Uint8>(255), static_cast<sf::Uint8>(255), static_cast<sf::Uint8>(255), static_cast<sf::Uint8>(50)));
+			shape.setFillColor(sf::Color(static_cast<sf::Uint8>(255), static_cast<sf::Uint8>(255), static_cast<sf::Uint8>(255), static_cast<sf::Uint8>(0)));
 			target.draw(shape);
 		}
 	}
+
 	std::random_device rd{};
 	std::mt19937 gen{ rd() };
-	std::normal_distribution<> d{ 0, 12 };
 
 	void initializeParticles(std::size_t index){
 		float angle = (std::rand() % 360) * 3.14f / 180.f;
@@ -174,20 +191,43 @@ private:
 
 	void resetParticle(std::size_t index)
 	{
-		//std::cout << "reset" << std::endl;
-		_emitterAngle = acos((emitterVelocity.x * 0 + emitterVelocity.y * 1) / (sqrt(emitterVelocity.x*emitterVelocity.x + emitterVelocity.y*emitterVelocity.y)))* 180.0 / 3.14;
-		if (emitterVelocity.x > 0 && emitterVelocity.y > 0)
-			_emitterAngle = _emitterAngle + 180;
-		if (emitterVelocity.x > 0 && emitterVelocity.y < 0)
-			_emitterAngle = _emitterAngle + 0;
-		if (emitterVelocity.x < 0 && emitterVelocity.y > 0)
-			_emitterAngle = _emitterAngle + 270;
+		float emitterVelocityNorm = sqrt(emitterVelocity.x*emitterVelocity.x + emitterVelocity.y*emitterVelocity.y);
+		if (emitterVelocity.x >= 0 && emitterVelocity.y > 0)
+		{
+			_emitterAngle = acos((emitterVelocity.x * 0 + emitterVelocity.y * 1) / emitterVelocityNorm)* 180.0 / 3.14;
+			_emitterAngle = 270 - _emitterAngle;
+		}
+		if (emitterVelocity.x >= 0 && emitterVelocity.y < 0)
+		{
+			_emitterAngle = acos((emitterVelocity.x * 1 + emitterVelocity.y * 0) / emitterVelocityNorm)* 180.0 / 3.14;
+			_emitterAngle = 180 - _emitterAngle;
+		}
+		if (emitterVelocity.x < 0 && emitterVelocity.y >= 0)
+		{
+			_emitterAngle = acos((-emitterVelocity.x * 1 + emitterVelocity.y * 0) / emitterVelocityNorm)* 180.0 / 3.14;
+			_emitterAngle = 0 - _emitterAngle;
+		}
 		if (emitterVelocity.x < 0 && emitterVelocity.y < 0)
-			_emitterAngle = _emitterAngle + 270;
-		float angle = ((static_cast<int>(d(gen)) % 360) * 3.14f / 180.f)+(_emitterAngle * 3.14f / 180.f);// (std::rand() % 6)
-		float speed = (std::rand() % 5) + 100.f;
+		{
+			_emitterAngle = acos((emitterVelocity.x * 0 - emitterVelocity.y * 1) / emitterVelocityNorm)* 180.0 / 3.14;
+			_emitterAngle = 90 - _emitterAngle;
+		}
+
+
+		float angle;
+		if (distribution == Distribution::Normal) {
+			std::normal_distribution<float> normal{ _mean, _sigma };
+			angle = ((static_cast<int>(normal(gen)) % 360) * 3.14f / 180.f) + (_emitterAngle * 3.14f / 180.f);
+
+		}
+		else
+		{
+			angle = (std::rand() % _range) * 3.14f / 180.f + (_emitterAngle * 3.14f / 180.f) - _range * 3.14f / 360.f;
+		}
+		float speed = (std::rand() % 5) + _speed;
+
 		m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-		m_particles[index].lifetime = sf::milliseconds((std::rand() % 500) +3000);
+		m_particles[index].lifetime = sf::milliseconds((std::rand() % 500) + _lifetime);
 		m_particles[index].AllLifetime = m_particles[index].lifetime;
 
 		m_vertices[index].position = m_emitter;
@@ -198,21 +238,27 @@ private:
 		sf::Vector2f centre;
 		float radius;
 	};
+
 	float _emitterAngle = 180;
-	int _rate = 0;
+	int _rate = 10;
 	sf::Vector2f _simpleForce;
 	std::vector<circleForce> _forces;
 	sf::Vector2u m_canvasSize;
 	std::vector<Particle> m_particles;
 	sf::VertexArray m_vertices;
 	sf::Time m_lifetime;
-	sf::Vector2f m_emitter;
-	sf::Vector2f emitterVelocity;
+	float _mean=0;
+	float _sigma=4;
+	int _range = 360;
+	int _lifetime = 3500;
+	float _speed = 100.f;
+	
+
 	std::vector<std::pair<float, sf::Color>> colors = { 
 	{ 1.f,sf::Color::Color(static_cast<sf::Uint8>(255),static_cast<sf::Uint8>(255),static_cast<sf::Uint8>(255),static_cast<sf::Uint8>(255)) },
-	{ .7f, sf::Color::Color(static_cast<sf::Uint8>(255),static_cast<sf::Uint8>(128),static_cast<sf::Uint8>(0),static_cast<sf::Uint8>(255))},
-	{ .6f, sf::Color::Red },
-	{ .2f, sf::Color::Color(static_cast<sf::Uint8>(100), static_cast<sf::Uint8>(100), static_cast<sf::Uint8>(100),static_cast<sf::Uint8>(255)) },
+	{ .9f, sf::Color::Color(static_cast<sf::Uint8>(255),static_cast<sf::Uint8>(128),static_cast<sf::Uint8>(0),static_cast<sf::Uint8>(255))},
+	{ .7f, sf::Color::Red },
+	{ .5f, sf::Color::Color(static_cast<sf::Uint8>(90), static_cast<sf::Uint8>(0), static_cast<sf::Uint8>(57),static_cast<sf::Uint8>(255)) },
 	{ .1f,  sf::Color::Black }
 	};
 };
--- Source/ParticleSystem/Soure.cpp
+++ Source/ParticleSystem/Soure.cpp
@@ -2,6 +2,38 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Particles.h"
+//
+class Rocket : public sf::Drawable {
+public:
+	ParticleSystem particles;
+
+	Rocket(unsigned int quantity, sf::Vector2u window):
+		particles(1000, window) {
+	}
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+	{
+		target.draw(particles);
+	}
+
+	void setPosition(sf::Vector2f position) {
+		particles.setEmitterPosition(position);
+	}
+
+	void setVelocity(sf::Vector2f velocity) {
+		particles.setEmitterVelocity(velocity);
+		particles.AddCircleForceForRocket();
+	}
+
+	void stop() {
+		particles.setRate(0);
+	}
+	void update(sf::Time deltaTime) {
+		particles.update(deltaTime);
+	}
+
+};
+
 
 int main()
 {
@@ -14,69 +46,86 @@ int main()
 	ParticleSystem particles(1000, window.getSize());
 	particles.setEmitter(sf::Vector2f(500, 500));
 	particles.setEmitterVelocity(sf::Vector2f(2, 2));
-	particles.AddCircleForce(sf::Vector2f(350, 450), 50, -2);
-	particles.AddCircleForce(sf::Vector2f(200, 450), 50, 2);
+	particles.distribution = Distribution::Normal;
 	particles.setRate(130);
 
 
 	ParticleSystem particles1(1000, window.getSize());
-	particles1.setEmitter(sf::Vector2f(350, 350));
-	particles1.setRate(10);
-	particles1.setEmitterVelocity(sf::Vector2f(20,5));
+	//particles1.setEmitter(sf::Vector2f(350, 350));
+	particles1.distribution = Distribution::Uniform;
+	//particles1.setRate(10);
+	//particles1.setEmitterVelocity(sf::Vector2f(-20,5));
+	//particles1.AddCircleForce(particles1.m_emitter - sf::Vector2f(2 * particles1.emitterVelocity.x + 15, 2 * particles1.emitterVelocity.y + 15), 20, -0.4);
+	//particles1.AddCircleForce(particles1.m_emitter - sf::Vector2f(4 * particles1.emitterVelocity.x + 15, 4 *  particles1.emitterVelocity.y + 20), 20, -0.4);
+	//particles1.AddCircleForce(particles1.m_emitter - sf::Vector2f(6 * particles1.emitterVelocity.x+10, 6 *      particles1.emitterVelocity.y + 10), 20, 0.8);
 
-	ParticleSystem particles2(100, window.getSize());
-	particles2.setEmitter(sf::Vector2f(350, 350));
-	particles2.setRate(10);
-	particles2.setEmitterVelocity(sf::Vector2f(-10, 20));
+	//ParticleSystem particles2(1000, window.getSize());
+	//particles2.setEmitter(sf::Vector2f(450, 450));
+	//particles2.setRate(10);
+	//particles2.setEmitterVelocity(sf::Vector2f(-10,0));
+	//particles2.distribution = Distribution::Uniform;
+	//particles2.setUniformDistrRange(90);
 
-	ParticleSystem particles3(100, window.getSize());
-	particles3.setEmitter(sf::Vector2f(350, 350));
-	particles3.setRate(10);
-	particles3.setEmitterVelocity(sf::Vector2f(20, -20));
+	//ParticleSystem particles3(100, window.getSize());
+	//particles3.setEmitter(sf::Vector2f(350, 350));
+	//particles3.setRate(10);
+	//particles3.setEmitterVelocity(sf::Vector2f(-20, 5));
+	//particles3.distribution = Distribution::Uniform;
 
-	ParticleSystem particles4(100, window.getSize());
-	particles4.setEmitter(sf::Vector2f(350, 350));
-	particles4.setRate(10);
-	particles4.setEmitterVelocity(sf::Vector2f(-14, -7));
+	//ParticleSystem particles4(100, window.getSize());
+	//particles4.setEmitter(sf::Vector2f(350, 350));
+	//particles4.setRate(10);
+	//particles4.setEmitterVelocity(sf::Vector2f(-14, -7));
 
 
 	sf::Clock clock;
 	sf::Time timer = clock.getElapsedTime();
 	sf::Time deltaTime;
 	
-
-
+	int count=0;
+	sf::Vector2f LastMousePos(0,0);
+	Rocket rocket(1000, window.getSize());
+	rocket.setPosition(sf::Vector2f(200, 233));
+	rocket.setVelocity(sf::Vector2f(-20, 5));
+	
 	while (window.isOpen())
 	{
-		
+
 		sf::Vector2f mousePos =
 			static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
 
-		
 		if (mousePos.x > 0
 			|| mousePos.y > 0
 			|| mousePos.x < window.getSize().x
 			|| mousePos.y < window.getSize().y)
-			particles.setEmitterPosition(mousePos);
+					particles.setEmitterPosition(mousePos);
+		
+		if (LastMousePos.x != 0 && LastMousePos.y != 0)
+			particles.setEmitterVelocity(mousePos - LastMousePos);
 
 		auto now = clock.getElapsedTime();
 		deltaTime = now - timer;
 		timer = now;
+		rocket.update(deltaTime * 1.f);
+		if (count == 100)
+			rocket.stop();
 
-		particles.update(deltaTime * 1.f);
+		//particles.update(deltaTime * 1.f);
 		particles1.update(deltaTime * 1.f);
-		particles2.update(deltaTime * 1.f);
+		/*particles2.update(deltaTime * 1.f);
 		particles3.update(deltaTime * 1.f);
 		particles4.update(deltaTime * 1.f);
-		
+		*/
 		window.clear(sf::Color::Black);
 		//window.draw(particles);
-
-		window.draw(particles1);
-		//window.draw(particles2);
+		window.draw(rocket);
+		//window.draw(particles1);
+		/*window.draw(particles2);*/
 		//window.draw(particles3);
 		//window.draw(particles4);
 		window.display();
+		LastMousePos = mousePos;
+		count++;
 	}
 	system("pause");
 	return 0;
