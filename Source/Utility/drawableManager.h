2aa1d3ab9fbc33ed38e6fb6e8fe9c4a49442ce1b
Alexander Osipov <alexander.osipov@sperasoft.com>
2018-04-25 16:17:27 +0300

    fixed utility project

--- Source/Utility/drawableManager.h
+++ Source/Utility/drawableManager.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "Drawable.h"
+#include <vector>
 #include <algorithm>
 
 class DrawableManager
 {
 private:
 	DrawableManager(DrawableManager const&) = delete;
-
 	DrawableManager& operator= (DrawableManager const&) = delete;
 
 	DrawableManager();
-
 	~DrawableManager();
 public:
 	std::vector<Drawable*> _drawableObjects;
